b311af13a63e641f1ef35745daa6cc1599c7cf3d
wdpfeffer <wdpfeffer@gmail.com>
2018-09-15 19:01:35 -0600

    current situation

--- boot.py
+++ boot.py
@@ -1,7 +1,18 @@
 import esp
-import network
+import webrepl
 
-sta = network.WLAN(network.STA_IF)
-sta.active(True)
-sta.ifconfig(('192.168.1.210', '255.255.255.0', '192.168.1.1', '192.168.1.1'))
-sta.connect('1-FBIsurveillance', 'tInKtHEGoAt06')
+esp.osdebug(None)
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    if not sta_if.isconnected():
+        print("Connecting...")
+        sta_if.ifconfig(('192.168.1.192', '255.255.255.0', '192.168.1.1', '8.8.8.8'))
+        sta_if.connect('1-FBIsurveillance', 'tInKtHEGoAt06')
+        while not sta_if.isconnected():
+            pass
+        print('network config:', sta_if.ifconfig())
+        webrepl.start()
+
+do_connect()
